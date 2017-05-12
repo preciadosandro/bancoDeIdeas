9050075dbfffbffd2f24c5ebc551921be356bde2
blamora <blamora88@gmail.com>
2017-05-11 21:01:34 -0500

    Refactorizado el codigo

--- edu.uniajc.logic/src/java/edu/uniajc/ideaBank/logic/services/UserService.java
+++ edu.uniajc.logic/src/java/edu/uniajc/ideaBank/logic/services/UserService.java
@@ -25,34 +25,34 @@ public class UserService implements IUser {
 
     Connection dbConnection;
     boolean validatorUser, validatorNumId;
-    User userModel;    
-    
+    User userModel;
+
+    public UserService() throws NamingException, SQLException {
+
+        this.dbConnection = ((DataSource) new InitialContext().lookup("jdbc/sample")).getConnection();
+    }
+
     @Override
-    public boolean createUser(User userModel) {
-        try {
-            dbConnection = ((DataSource) new InitialContext().lookup("jdbc/sample")).getConnection();
-            UserDAO dao = new UserDAO(dbConnection);
-            validatorUser = dao.getUserByUser(userModel.getUsuario());
-            validatorNumId = dao.getUserById(userModel.getNumIdentificacion());
-            if (validatorUser == false && validatorNumId == false) {
+    public int createUser(User userModel) {
+        UserDAO dao = new UserDAO(dbConnection);
+        boolean confirmUser;
+        validatorUser = dao.getUserByUser(userModel.getUsuario());
+        validatorNumId = dao.getUserById(userModel.getNumIdentificacion());
+        if (validatorUser == false) {
+            if (validatorNumId == false) {
                 userModel.setContrasena(Utilities.Encriptar(userModel.getContrasena()));
                 userModel.setIdEstadoUsuario(1);
-                return dao.createUser(userModel);
-            } else {
-                return false;
-            }
-        } catch (SQLException | NamingException e) {
-            System.out.println(e.getMessage());
-            return false;
-        } finally {
-            try {
-                if (null != dbConnection) {
-                    dbConnection.close();
+                confirmUser = dao.createUser(userModel);
+                if (confirmUser == true) {
+                    return 0;
+                } else {
+                    return -1;
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            }else{
+                return -2;
             }
-
+        }else{
+            return -3;
         }
     }
 }
